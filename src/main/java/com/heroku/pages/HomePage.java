cdab98e69002d8da3fe6cc0e657132cd48d5d54f
Olga-2707 <olya2707ua@gmail.com>
2026-03-09 23:09:54 +0200

    Initial commit

---
+++ src/main/java/com/heroku/pages/HomePage.java
@@ -0,0 +1,14 @@
+package com.heroku.pages;
+
+import com.heroku.core.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage extends BasePage {
+
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+
+}
---
+++ src/test/java/com/heroku/core/TestBase.java
@@ -0,0 +1,20 @@
+package com.heroku.core;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class TestBase {
+    protected WebDriver driver;
+
+    @BeforeEach
+    public void init(){
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+}
